a43650039a0db628b73a91a7ca65a614fa5216af
leeh24 <leeh24@up.edu>
2023-10-22 11:55:51 -0700

    load data from service endpoint.

--- src/views/ElectricDetails.js
+++ src/views/ElectricDetails.js
@@ -1,13 +1,47 @@
-import React from "react";
+import React, {useEffect,useState} from "react";
 import Header from "../components/Header";
+import axios from 'axios';
+
 
 export default function ElectricalDetails() {
+    const [sensorData, setSensorData] = useState([]);
+
+    const headers = {
+        'Access-Control-Allow-Origin': '*',
+        'Content-Type': 'application/json',
+    };
+
+    const loaddata = () => {
+        alert("Great Shot!");
+        const apiData =  axios.get("http://127.0.0.1:5000/sensors", {headers})
+        .then (res => {
+            console.log(res.data);
+            setSensorData(res.data);
+        });
+    }
+
+          {/* call service endpoint to load data 
+    useEffect(() => {
+        axios.get("http://127.0.0.1:5000/sensors", {headers})
+            .then (res => {
+                console.log(res.data);
+                setSensorData(res.data);
+            });
+    },[]);
+      }
+
+  */}
+
     return (
         <>
             <Header />
 
             <div>
                 <h1>Electrical Details</h1>
+            </div>
+
+            <div>
+                <button onClick={loaddata}>Load Data</button>
             </div>
         </>
     );
