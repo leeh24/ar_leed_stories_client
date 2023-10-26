32526208ee98fb14ea713b16f69b92c6bb09bd3d
leeh24 <leeh24@up.edu>
2023-10-25 23:30:30 -0700

    implement electricity page

--- src/views/ElectricDetails.js
+++ src/views/ElectricDetails.js
@@ -1,74 +1,41 @@
 import React, {useEffect,useState} from "react";
 import Header from "../components/Header";
-import axios from 'axios';
-
+import lightBackground from "../images/lightBackground.jpg"
 
 export default function ElectricalDetails() {
-    const [sensorData, setSensorData] = useState([]);
-    const [error, setError] = useState(null);
-
-
-
-    function loadData() {
-        axios.get(`http://localhost:5000/sensors`)
-        .then (res => {
-            console.log(res.data);
-            setSensorData(res.data);
-            setError(null);
-        })
-        .catch((err) => { 
-            console.log(err);
-            setError(err);
-        });
-    }
-      
-    useEffect(() => {
-        loadData();
-    }, []);
-
- 
-    const callLoadData = () => {
-        loadData();
-    }
 
     return (
         <>
             <Header />
-
-            <div>
-                <h1>Electrical Usage</h1>
-            </div>
-
-            <div>
-                <button onClick={callLoadData}>Reload Data</button>
-            </div>
-
-            <br />
-            {
-                error != null &&
-                    <div className="error-msg">Error loading data: {error.message}</div>
-            }
-
-            {
-                sensorData.map(sensor => (
-                    <div className="data-section">
-                        {console.log(sensor)}
-                        <div className="data-title">
-                            <div>Sensor Name: {sensor.name}</div>
-                            <div>Sensor Description: {sensor.description}</div>
-                            <div>Room: {sensor.room}</div>
-                        </div>
-                        <ul>
-                        {
-                            sensor.data.map(sData => ( 
-                                <li>{sData.datetime} : {sData.value}{sData.units}</li>
-                            ))
-                        }
-                        </ul>
-                    </div>
-                ))
+            <body style={{ backgroundImage:`url(${lightBackground})`, backgroundRepeat:"no-repeat",backgroundSize:"cover", height:"700px"}}>
+                <div >
+                    <h1 style={{textAlign:"center"}}>Electrical Usage</h1>
+                </div>
+                <br></br>
+                <br></br>
                 
-            }
+                    <div className="wattsCounter">
+                        70 Kilowatts
+                    </div>
+               
+                <br></br>
+                <div className="electricInfo">
+                        Information on how the building uses the electricity, and its systems.
+                </div>
+                
+                <div className="floorButtonContainer">
+                    <button className="floorButton">Floor 1</button>
+                    <br></br>
+                    <button className="floorButton">Floor 2</button>
+                    <br></br>
+                    <button className="floorButton">Floor 3</button>
+                </div>
+                <br></br>
+                <br></br>
+                <br></br>
+                <br></br>
+            </body>
+
            
         </>
     );
