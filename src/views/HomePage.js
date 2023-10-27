bbb914cfa720520ceb06204a3e4d25396b9a03e4
KeoniHan2024 <han24@up.edu>
2023-10-26 22:32:28 -0700

    changed some extra issues

--- src/views/HomePage.js
+++ src/views/HomePage.js
@@ -26,9 +26,8 @@ export default function HomePage() {
                         </div>
                     </div>  
                     <div class="bottom-container">
-                    <div class="containerText">Shiley Marcos Center’s purpose was to provide adaptable lab environments, create a third hub on campus, and provide experimental learning opportunities. This building is filled with many systems that promote sustainability and achieve LEED certification. The systems include the HVAC, solar panels, radiant slabs, and more. 
-                        To visualize how these systems work and promote sustainability, Opsis, Skanska, and the faculty came up with an idea of an AR dashboard, which is similar to the normal dashboard we had here in Shiley school of engineering. Dean Fabien’s vision of this dashboard was for it to be a product “for students by students”. The dashboard would allow students to scan areas and see data from the different sensors and meters, hoping that professors can use this for educational purposes as well. Last year’s group developed an API that can take data from a fake building automation system, and our goal is to build on top of this to take real data from the online systems in the Shiley Marcos Building. 
-                        </div>
+                    <div class="containerText">Shiley Marcos Center’s purpose is to provide adaptable lab environments, provide experimental learning opportunities, and establish third hub right here on campus. This building is filled with many systems that promote sustainability and achieve LEED certification. Theses systems include the HVAC system, solar panels, radiant slabs, and more. 
+                                </div>
                     </div>
                 </div>
             
