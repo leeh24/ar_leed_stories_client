5620098bb6310c6af59a4f7f828422e5d56bbaf3
KeoniHan2024 <han24@up.edu>
2023-11-02 22:36:40 -0700

    Updated header to be compatible on more devices

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,39 +9,53 @@ export default function Header() {
     const navigate = useNavigate();
     const [isSidebarOpen, setSidebarOpen] = useState(false);
     return (
-        <>
-            <div className="App-header">
-                <div className="building-name">
+    <>
+        <header>
+        <div class="inner">
+            <div class="logo">
+                <div>
                     <Link to="/">
                         <img src={logo}></img>
                     </Link> 
                 </div>
-                {/* Desktop Menu */}
-                <div className="menu-container">
-                    <div className="menu-bar">
-                        <NavLink to="/leed-stories" activeClassName="activeLink" className="navButton">
-                            LEED Stories
-                        </NavLink>
-                        <NavLink to="/electric-details" activeClassName="activeLink" className="navButton">
-                            Electricity
-                        </NavLink>
-                        <NavLink to="/humidityTemp-details" activeClassName="activeLink" className="navButton">
-                            Humidity/Temperature
-                        </NavLink>
-                        <NavLink to="/water-details" activeClassName="activeLink" className="navButton">
-                            Water
-                        </NavLink>
-                        <NavLink to="/air-details" activeClassName="activeLink" className="navButton">
-                            Air Quality
-                        </NavLink>
-                        <NavLink to="/solar-details" activeClassName="activeLink" className="navButton">
-                            Solar
-                        </NavLink>
-                    </div>
-                </div>
             </div>
 
-                    {/* Mobile Menu */}
+            <nav>
+                <li><span>
+                    <NavLink to="/leed-stories" activeClassName="activeLink" className="navButton">
+                        LEED Stories
+                    </NavLink>
+                </span></li>
+                <li><span>
+                    <NavLink to="/electric-details" activeClassName="activeLink" className="navButton">
+                        Electricity
+                    </NavLink>
+                </span></li>
+                <li><span>
+                    <NavLink to="/humidityTemp-details" activeClassName="activeLink" className="navButton">
+                        Humidity/Temperature
+                    </NavLink>
+                </span></li>
+                <li><span>
+                    <NavLink to="/water-details" activeClassName="activeLink" className="navButton">
+                        Water
+                    </NavLink>
+                </span></li>
+                <li><span>
+                    <NavLink to="/air-details" activeClassName="activeLink" className="navButton">
+                        Air Quality
+                    </NavLink>
+                </span></li>
+                <li><span>
+                    <NavLink to="/solar-details" activeClassName="activeLink" className="navButton">
+                        Solar
+                    </NavLink>
+                </span></li>
+            </nav>
+        </div>
+    </header>
+
+
                     <button className="sidebarButton" onClick={() => setSidebarOpen(!isSidebarOpen)}>
                         <img className="menuButtonImage" src={menuButton}></img>
                     </button>
