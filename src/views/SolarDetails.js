df70a5fdd441847484e825e38312189f672c28b5
priced24 <priced24@up.edu>
2023-11-17 14:41:03 -0800

    air quality page work, add-ons to solar page

--- src/views/SolarDetails.js
+++ src/views/SolarDetails.js
@@ -4,7 +4,7 @@ import {
     BarElement,
     LineElement,
     PointElement,
-    ArcElement, 
+    ArcElement,
     CategoryScale,
     LinearScale,
     Tooltip,
@@ -12,7 +12,7 @@ import {
 } from "chart.js";
 import { Bar, Line, Doughnut, Pie } from "react-chartjs-2";
 import Header from "../components/Header";
-// import bgImage from "../images/bg.png";
+import bgImage from "../images/bg.png";
 
 ChartJS.register(
     BarElement,
@@ -26,23 +26,79 @@ ChartJS.register(
 )
 
 export default function SolarDetails() {
-    const barData = {
-        labels: ['Mon', 'Tue', 'Wed'],
+    /* const barData = {
+        labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
         datasets: [{
             label: 'test',
             data: [3, 6, 9],
+            data: [
+                { id: 'Mon', value:
+                    {
+                        2022: { consumed: 100, generated: 200 },
+                        2023: { consumed: 290, generated: 350 }
+                    }
+                },
+                { id: 'Tue', value:
+                    {
+                        2022: { consumed: 160, generated: 170 },
+                        2023: { consumed: 340, generated: 350 }
+                    }
+                },
+                { id: 'Wed', value:
+                    {
+                        2022: { consumed: 320, generated: 500 },
+                        2023: { consumed: 230, generated: 350 }
+                    }
+                },
+                { id: 'Thu', value:
+                    {
+                        2022: { consumed: 190, generated: 275 },
+                        2023: { consumed: 500, generated: 500 }
+                    }
+                },
+                { id: 'Fri', value:
+                    {
+                        2022: { consumed: 400, generated: 485 },
+                        2023: { consumed: 290, generated: 400 }
+                    }
+                },
+                { id: 'Sat', value:
+                    {
+                        2022: { consumed: 395, generated: 525 },
+                        2023: { consumed: 230, generated: 310 }
+                    }
+                },
+                { id: 'Sun', value:
+                    {
+                        2022: { consumed: 460, generated: 540 },
+                        2023: { consumed: 600, generated: 700 }
+                    }
+                }
+            ],
             backgroundColor: 'aqua',
             borderColor: 'black',
             borderWidth: 1,
         }]
-    }
+    } 
 
-    const barOptions = {
+    const barConfig = {
+        type: 'bar',
+        barData,
+        options: {
+            parsing: {
+                xAxisKey: 'id',
+                yAxisKey: 'value.2022.consumed'
+            },
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    } */
 
-    }
-
-    const lineData = {
-        labels: ['Mon', 'Tue', 'Wed'],
+    /* const lineData = {
+        labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
         datasets: [{
             label: 'test',
             data: [3, 6, 9],
@@ -82,7 +138,103 @@ export default function SolarDetails() {
 
     const pieOptions = {
 
+    } */
+
+    const chartData = {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        datasets: [
+            {
+                label: 'Sample Data',
+                data: [65, 59, 80, 81, 56, 55, 40],
+                /*data: [
+                    { id: 'Mon', value:
+                        {
+                            2022: { consumed: 100, generated: 200 },
+                            2023: { consumed: 290, generated: 350 }
+                        }
+                    },
+                    { id: 'Tue', value:
+                        {
+                            2022: { consumed: 160, generated: 170 },
+                            2023: { consumed: 340, generated: 350 }
+                        }
+                    },
+                    { id: 'Wed', value:
+                        {
+                            2022: { consumed: 320, generated: 500 },
+                            2023: { consumed: 230, generated: 350 }
+                        }
+                    },
+                    { id: 'Thu', value:
+                        {
+                            2022: { consumed: 190, generated: 275 },
+                            2023: { consumed: 500, generated: 500 }
+                        }
+                    },
+                    { id: 'Fri', value:
+                        {
+                            2022: { consumed: 400, generated: 485 },
+                            2023: { consumed: 290, generated: 400 }
+                        }
+                    },
+                    { id: 'Sat', value:
+                        {
+                            2022: { consumed: 395, generated: 525 },
+                            2023: { consumed: 230, generated: 310 }
+                        }
+                    },
+                    { id: 'Sun', value:
+                        {
+                            2022: { consumed: 460, generated: 540 },
+                            2023: { consumed: 600, generated: 700 }
+                        }
+                    }
+                ], */
+                backgroundColor: 'rgba(75,192,192,0.2)',
+                borderColor: 'rgba(75,192,192,1)',
+                borderWidth: 1,
+            },
+        ],
+    };
+
+    const [graphType, setGraphType] = useState('bar'); // initial graph
+
+    const handleGraphChange = (e) => {
+        console.log(e.target.value);
+        setGraphType(e.target.value);
     }
+
+    const renderGraph = () => {
+        switch (graphType) {
+            case "bar":
+                return <Bar data={chartData} />;
+            case "line":
+                return <Line data={chartData} />;
+            default:
+                return <Bar data={chartData} />;
+        }
+    }
+
+
+    const [chartType, setChartType] = useState('doughnut'); // initial chart
+
+    const handleChartChange = (e) => {
+        console.log(e.target.value);
+        setChartType(e.target.value);
+    };
+
+    const renderChart = () => {
+        switch (chartType) {
+            case "doughnut":
+                return <Doughnut data={chartData} />;
+            case "pie":
+                return <Pie data={chartData} />;
+            default:
+                return <Doughnut data={chartData} />;
+        }
+    }
+
+
 
     return (
         <>
@@ -103,48 +255,30 @@ export default function SolarDetails() {
                     <h3>Analyze solar energy data</h3>
 
                     <select id="dataTimeInterval">
-                        <option>Today</option>
-                        <option>This week</option>
-                        <option>This month</option>
-                        <option>This year</option>
+                        <option value="daily">Today</option>
+                        <option value="weekly">This week</option>
+                        <option value="monthly">This month</option>
+                        <option value="yearly">This year</option>
                     </select>
 
                     <select id="dataType">
-                        <option>Energy Consumed</option>
-                        <option>Energy Generated</option>
+                        <option value="consumed">Energy Consumed</option>
+                        <option value="generated">Energy Generated</option>
                     </select>
 
-                    <select id="graphType">
-                        <option>Bar Graph</option>
-                        <option>Line Graph</option>
+                    <select value={graphType} onChange={handleGraphChange}>
+                        <option value="bar">Bar Graph</option>
+                        <option value="line">Line Graph</option>
                     </select>
 
-                    <select id="chartType">
-                        <option>Doughnut Chart</option>
-                        <option>Pie Chart</option>
+                    <select value={chartType} onChange={handleChartChange}>
+                        <option value="doughnut">Doughnut Chart</option>
+                        <option value="pie">Pie Chart</option>
                     </select>
 
-                    <Bar
-                        data={barData}
-                        options={barOptions}
-                    ></Bar>
-
-                    <Line
-                        data={lineData}
-                        options={lineOptions}
-                    ></Line>
-
-                    <Doughnut
-                        data={doughnutData}
-                        options={doughnutOptions}
-                    ></Doughnut>
-
-                    <Pie
-                        data={pieData}
-                        options={pieOptions}
-                    ></Pie>
+                    {renderGraph()}
+                    {renderChart()}
                 </div>
-
             </body>
         </>
     );
