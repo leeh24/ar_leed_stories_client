2e62414a685eeeb8f854fd379f750b96d1479497
KeoniHan2024 <han24@up.edu>
2024-02-04 15:54:55 -0800

    added buttons

--- src/views/imageSlider.js
+++ src/views/imageSlider.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { SliderData } from './sliderData';
 import {FaArrowAltCircleRight, FaArrowAltCircleLeft} from 'react-icons/fa';
+import { CgScrollV } from "react-icons/cg";
 
 const ImageSlider = ({ slides }) => { 
     const [current, setCurrent] = useState(0);
@@ -22,11 +23,20 @@ const ImageSlider = ({ slides }) => {
         setCurrent(current === 0 ? length - 1 : current - 1);
     };
 
+    const scrollDown = () => {
+        window.scrollTo({
+            top: document.body.scrollHeight,
+            behavior: 'smooth'
+        });
+    };
+
+
     return (
         <>
             <FaArrowAltCircleLeft className='left-arrow' onClick={ prevSlide } />
             <FaArrowAltCircleRight className='right-arrow' onClick={ nextSlide } />
-            
+            <CgScrollV className='scroll-icon' onClick={ scrollDown } />
+
             {SliderData.map((slide, index) => {
                 return <img src = {slide.image} alt = 'Shiley Photo' className={`Homeimage ${index === current ? 'fade-in' : 'fade-out'}`} />;
             })}
