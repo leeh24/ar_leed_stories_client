d1bd82de2af972a295cf8545522ec5c688ee4abb
KeoniHan2024 <han24@up.edu>
2024-03-13 15:40:08 -0700

    removed unecessary functions

--- src/views/imageSlider2.js
+++ src/views/imageSlider2.js
@@ -15,20 +15,6 @@ const ImageSlider2 = ({ slides }) => {
     }, [length]);
 
 
-    const nextSlide = () => {
-        setCurrent(current === length - 1 ? 0 : current + 1);
-    };
-    const prevSlide = () => {
-        setCurrent(current === 0 ? length - 1 : current - 1);
-    };
-
-    const scrollDown = () => {
-        window.scrollTo({
-            top: document.body.scrollHeight,
-            behavior: 'smooth'
-        });
-    };
-
 
     return (
         <>
